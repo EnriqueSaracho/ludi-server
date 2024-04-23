cf2c02e017ecae50865ca1ccf1aa4dd0c615e946
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-04-23 16:04:49 -0700

    Last commit failed, reverted changes
    src/index.js modified

--- src/index.js
+++ src/index.js
@@ -15,12 +15,7 @@ const app = express();
 
 // Adding middlewares.
 app.use(express.json());
-// app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 // Mounting routers to respective paths.
 app.use("/games", gamesRouter);
