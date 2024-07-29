5a4b1e5b727637b908954c9a5023dea5232e5d6d
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-07-28 17:28:35 -0700

    Added more route handlers to igdb.js

--- src/routes/igdb.js
+++ src/routes/igdb.js
@@ -11,19 +11,22 @@ const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 // Object: router.
 const router = express.Router();
 
-// Route handler: fetches a game's name and cover id from IGDB database
+// Route handler: fetches a number of games' name and cover id from IGDB database
 router.post("/games", async (req, res) => {
-  // console.log(req.body.query);
+  // const query = `fields name, cover; limit 500; search "${req.body.query}"; where cover != null & category = 0 & parent_game = null & version_parent = null;`;
+  const query = `fields name, cover; limit 500; search "${req.body.query}"; where cover != null;`; // TODO: Use above query for displaying only main titles
+
   try {
-    var options = {
+    const options = {
       method: "POST",
       url: "https://api.igdb.com/v4/games/",
       headers: {
         "Client-ID": CLIENT_ID,
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-      data: req.body.query,
+      data: query,
     };
+
     const response = await axios.request(options);
     res.json(response.data);
   } catch (err) {
@@ -31,8 +34,34 @@ router.post("/games", async (req, res) => {
   }
 });
 
-// Rounte handler: fetches the cover's url from IGDB database
+// Route handler: fetches a game initial data from IGDB database
+router.post("/game", async (req, res) => {
+  const query = `fields name, cover, rating, first_release_date, artworks, aggregated_rating, aggregated_rating_count, category; where id = ${req.body.query};`;
+  try {
+    const options = {
+      method: "POST",
+      url: "https://api.igdb.com/v4/games/",
+      headers: {
+        "Client-ID": CLIENT_ID,
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      data: query,
+    };
+
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
+
+// Rounte handler: fetches a number of cover image_id URLs from IGDB database
 router.post("/covers", async (req, res) => {
+  const gameRecords = req.body.query;
+  const query = `fields image_id; limit 500; where id = (${gameRecords
+    .map((record) => record.cover.id)
+    .join(",")});`;
+
   try {
     var options = {
       method: "POST",
@@ -41,7 +70,7 @@ router.post("/covers", async (req, res) => {
         "Client-ID": CLIENT_ID,
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-      data: req.body.query,
+      data: query,
     };
     const response = await axios.request(options);
     res.json(response.data);
@@ -50,18 +79,19 @@ router.post("/covers", async (req, res) => {
   }
 });
 
-// Rounte handler: fetches the cover's url from IGDB database
-router.post("/release_dates", async (req, res) => {
-  //fields date, date; where id
+// Rounte handler: fetches the cover's image_id URL from IGDB database
+router.post("/cover", async (req, res) => {
+  const query = `fields image_id; where id = ${req.body.query};`;
+
   try {
     var options = {
       method: "POST",
-      url: "https://api.igdb.com/v4/release_dates",
+      url: "https://api.igdb.com/v4/covers",
       headers: {
         "Client-ID": CLIENT_ID,
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-      data: req.body.query,
+      data: query,
     };
     const response = await axios.request(options);
     res.json(response.data);
@@ -70,9 +100,12 @@ router.post("/release_dates", async (req, res) => {
   }
 });
 
-// Rounte handler: fetches the cover's url from IGDB database
+// Rounte handler: fetches the game artworks' image_id URL from IGDB database
 router.post("/artworks", async (req, res) => {
-  //fields date, date; where id
+  const query = `fields image_id; limit 500; where id = (${req.body.query
+    .map((artwork) => artwork.id)
+    .join(",")});`;
+
   try {
     var options = {
       method: "POST",
@@ -81,7 +114,7 @@ router.post("/artworks", async (req, res) => {
         "Client-ID": CLIENT_ID,
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-      data: req.body.query,
+      data: query,
     };
     const response = await axios.request(options);
     res.json(response.data);
